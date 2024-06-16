0b7a5d63d06a5796cc7318313edc847dce65d41d
Pavel Ring <pavelpolosin@gmail.com>
2024-06-16 09:27:24 +0300

    Day 5 challange

--- Week2/Day5/Day5 challange/challange.py
+++
@@ -1,7 +0,0 @@
-user_input = input("Enter few words separated by coma: ")
-
-user_input_list = list(user_input.split(","))
-user_input_sorted_list = sorted(user_input_list)
-user_sorted_input_string = ",".join(user_input_sorted_list)
-
-print(user_sorted_input_string)
--- Week2/Day5/challange/challange.py
+++ Week2/Day5/challange/challange.py
@@ -1,4 +1,7 @@
 user_input = input("Enter few words separated by coma: ")
 
-user_input_list = list(user_input)
-print(user_input_list)
+user_input_list = list(user_input.split(","))
+user_input_sorted_list = sorted(user_input_list)
+user_sorted_input_string = ",".join(user_input_sorted_list)
+
+print(user_sorted_input_string)
